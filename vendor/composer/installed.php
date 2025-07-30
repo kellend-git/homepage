65803468da3ebb0aaaf01c9e96e79f6cd38259d0
Kellen Donohue <melonhead901@gmail.com>
2025-07-29 23:22:23 -0700

    update to new serving

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6f94770f7ee5f1a159fe53b7413d7ac8dbf0573e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6f94770f7ee5f1a159fe53b7413d7ac8dbf0573e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
